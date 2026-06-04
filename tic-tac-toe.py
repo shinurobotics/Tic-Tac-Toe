376f6ade9ad3efc21f88670ddc3be520d9e22042
shinurobotics <shinu0848@gmail.com>
2026-06-04 20:57:59 +0800

    Create tic-tac-toe.py

---
+++ tic-tac-toe.py
@@ -0,0 +1,21 @@
+#   | |    0R
+#  -----   1R
+#   | |    2R
+#  -----   3R
+#   | |    4R
+#c 01234
+
+def drawField():
+    for row in range(5):
+        if row%2 == 0:
+            for column in range(5):
+                if column%2 == 0:
+                    if column != 4:
+                        print(" ",end = "")
+                    else:
+                        print(" ")
+                else:
+                    print("|",end = "")
+        else:
+            print("-----")
+drawField()
