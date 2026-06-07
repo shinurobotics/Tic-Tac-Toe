a9eab305b4b0fc45521c7385161f195164f2d4ac
shinurobotics <shinu0848@gmail.com>
2026-06-07 17:37:25 +0800

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,5 +18,19 @@ def drawField():
                     print("|",end = "")
         else:
             print("-----")
-drawField()
-stops at 14:33
+Player = 1
+currentField = [[" "," "," "],[" "," "," "],[" "," "," ",]]
+print(currentField)
+while(True):
+    print("Players Turn:",Player)
+    MoveRow = int(input("Please enter the row\n"))
+    MoveColumn = int(input("Please enter the column\n"))
+    if Player == 1:
+        #make move for Player 1
+        currentField[MoveColumn][MoveRow] = "X"
+        Player = 2
+    else:
+        #make move for Player 2
+        currentField[MoveColumn][MoveRow] = "O"
+        Player = 1
+    print(currentField)
