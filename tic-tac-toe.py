e533fa5c9367c20b5a1777cd3af65e26e4497a73
shinurobotics <shinu0848@gmail.com>
2026-06-08 17:43:48 +0800

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,36 +1,42 @@
 #   | |    0R
-#  -----   1R
+#  -|-|-   1R
 #   | |    2R
-#  -----   3R
+#  -|-|-   3R
 #   | |    4R
 #c 01234
 
-def drawField():
-    for row in range(5):
-        if row%2 == 0:
-            for column in range(5):
-                if column%2 == 0:
+def drawField(field):
+    for row in range(5): #0,1,2,3,4
+                         #0,.,1,.,2
+        if row%2 == 0: #0,2,4
+            practicalRow = int(row/2)
+            for column in range(5): #0,1,2,3,4,
+                                    #0,.,1,.,2
+                if column%2 == 0: #0,2,4
+                    practicalColumn = int(column/2)
                     if column != 4:
-                        print(" ",end = "")
+                        print(field[practicalColumn][practicalRow],end ="")
                     else:
-                        print(" ")
+                        print(field[practicalColumn][practicalRow])
                 else:
                     print("|",end = "")
         else:
-            print("-----")
+            print("-|-|-")
 Player = 1
 currentField = [[" "," "," "],[" "," "," "],[" "," "," ",]]
-print(currentField)
+drawField(currentField)
 while(True):
     print("Players Turn:",Player)
     MoveRow = int(input("Please enter the row\n"))
     MoveColumn = int(input("Please enter the column\n"))
     if Player == 1:
         #make move for Player 1
-        currentField[MoveColumn][MoveRow] = "X"
-        Player = 2
+        if currentField[MoveColumn][MoveRow] == " ":
+            currentField[MoveColumn][MoveRow] = "X"
+            Player = 2
     else:
         #make move for Player 2
-        currentField[MoveColumn][MoveRow] = "O"
-        Player = 1
-    print(currentField)
+        if currentField[MoveColumn][MoveRow] == " ":
+            currentField[MoveColumn][MoveRow] = "O"
+            Player = 1
+    drawField(currentField)
